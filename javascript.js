c19d76e2691cf2f3c0c494fb4ccb8b86c3f72b46
unknown <devshrirathorework@gmail.com>
2024-12-19 23:54:02 +0530

    all file update

--- javascript.js
+++ javascript.js
@@ -2,3 +2,5 @@ console.log("git-practice");
 
 var abcd;
 console.log("hello")
+
+console.log("second")
--- js2.js
+++ js2.js
@@ -2,3 +2,6 @@ console.log("hello devshri rathore")
 var a,b;
 var sum = a+b;
 console.log("sum"+ sum);
+
+
+console.log("second")
